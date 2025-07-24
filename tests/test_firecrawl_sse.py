57bb3757a0c436168302b9dc755c96c914ce244f
karimdabbouz <hey@karim.ooo>
2025-07-24 07:54:20 +0200

    edit test

--- tests/test_firecrawl_sse.py
+++ tests/test_firecrawl_sse.py
@@ -4,7 +4,7 @@ import logfire
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pydantic_ai import Agent
-from pydantic_ai.mcp import MCPServerSSE, MCPServerHTTP, MCPServerStreamableHTTP
+from pydantic_ai.mcp import MCPServerSSE, MCPServerHTTP, MCPServerStreamableHTTP, MCPServerStdio
 import asyncio
 from dotenv import load_dotenv
 from schemas import OutputText
@@ -21,12 +21,21 @@ if not FIRECRAWL_API_KEY:
     raise ValueError('FIRECRAWL_API_KEY not found in .env')
 FIRECRAWL_URL = f'https://mcp.firecrawl.dev/{FIRECRAWL_API_KEY}/sse'
 
-server = MCPServerHTTP(FIRECRAWL_URL)
+# server = MCPServerSSE(url=FIRECRAWL_URL)
+# server = MCPServerStreamableHTTP(FIRECRAWL_URL)
+# server = MCPServerHTTP(FIRECRAWL_URL)
+server = MCPServerStdio(  
+    'npx',
+    args=[
+        '-y',
+        'firecrawl-mcp'
+    ]
+)
 agent = Agent('openai:gpt-4o-mini', toolsets=[server], output_type=OutputText)
 
 async def main():
     async with agent:
-        result = await agent.run('get the content of the wikipedia article on dortmund.')
+        result = await agent.run('get the content of the wikipedia article on paderborn and summarize it in maximum five sentences.')
     print(result.output)
 
 if __name__ == '__main__':
